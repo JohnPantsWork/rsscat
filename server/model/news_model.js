72a3480725d48665e090ea7a08d4427db4e2fb93
johnpantswork <johnpantswork@gmail.com>
2022-04-26 01:35:06 +0800

    feat: get news

--- server/model/news_model.js
+++ server/model/news_model.js
@@ -1,11 +1,20 @@
-const pool = require('../../util/rdb');
+const { pool } = require('../../util/rdb');
 
-async function getLatestNews(amount = 3, page = 0) {
+async function getLatestNews(page, amount) {
   try {
     const start = page * amount;
     const [result] = await pool.query(
-      'SELECT title,source,auther,des,picture,url FROM news_data ORDER BY id DESC LIMIT ? OFFSET ? ',
-      [start, amount]
+      `
+    SELECT nd.id, title, source, auther, des, picture, url, tag_id_1, tag_id_2, tag_id_3, ti.tag_name AS tag1, ti2.tag_name AS tag2, ti3.tag_name AS tag3
+    FROM news_data AS nd 
+      JOIN tag_info AS ti 
+        ON ti.id = nd.tag_id_1 
+      JOIN tag_info AS ti2
+        ON ti2.id = nd.tag_id_2
+      JOIN tag_info AS ti3
+        ON ti3.id = nd.tag_id_3
+        ORDER BY nd.id DESC LIMIT ? OFFSET ?;`,
+      [amount, start]
     );
     return result;
   } catch (err) {
@@ -14,19 +23,21 @@ async function getLatestNews(amount = 3, page = 0) {
   }
 }
 
-// tags = ['string-tagid','string-tagid']
-async function getTagNews(tags, amount = 3, page = 0) {
+async function seleteFeedNews(page, amount, tags) {
   try {
     const start = page * amount;
     const [result] = await pool.query(
-      'SELECT title,source,auther,des,picture,url FROM news_data ORDER BY id DESC LIMIT ? OFFSET ? WHERE tag1 in ',
-      [start, amount]
+      `SELECT DISTINCT id,title,auther,des,picture,url FROM news_data AS rs WHERE tag_id_1 in (?) OR tag_id_2 in (?) OR tag_id_3 in (?) ORDER BY id DESC LIMIT ? OFFSET ?`,
+      [tags, tags, tags, amount, start]
     );
-    return result;
+
+    const noDuplicatResult = [...new Set(result)];
+    console.log(`#noDuplicatResult#`, noDuplicatResult);
+    return noDuplicatResult;
   } catch (err) {
     console.error(err);
     return false;
   }
 }
 
-module.exports = { getLatestNews, getTagNews };
+module.exports = { getLatestNews, seleteFeedNews };
--- server/routes/news_route.js
+++ server/routes/news_route.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 
 const { wrapAsync } = require('../../util/util');
-const {} = require('../controller/news_controller');
+const { getExploreNews, getFeedNews } = require('../controller/news_controller');
+const { sessionCheck } = require('../controller/user_controller');
+
+router.route('/news').get(sessionCheck, wrapAsync(getExploreNews));
+router.route('/news/user').get(sessionCheck, wrapAsync(getFeedNews));
 
 module.exports = router;
